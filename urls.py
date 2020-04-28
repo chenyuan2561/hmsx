72b6ae963b35ed787b6c52b7bcdd971df0d22ee5
Bruce <chen054395@163.com>
2020-04-28 08:45:34 +0800

    '1次提交'

---
+++ urls.py
@@ -0,0 +1,11 @@
+from application import app
+from web.controllers.user.User import router_user
+from web.controllers.index import router_index
+
+# 拦截器的路由
+from web.interceptos.AuthInterceptor import *
+
+# 蓝图路由
+app.register_blueprint(router_user, url_prefix='/user')
+app.register_blueprint(router_index,url_prefix="/")
+app.register_blueprint(router_account,url_prefix="/account")
---
+++ web/controllers/account/Account.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+
+from common.libs.Helper import ops_render
+from common.models.User import User
+
+router_account = Blueprint("account_page",__name__)
+
+@router_account.route("/index")
+def index():
+    resp_data = {}
+    list = User.query.all()
+    resp_data['list'] = list
+    return ops_render("/account/index.html",resp_data)
+
+@router_account.route("/info")
+def info():
+    return ops_render("/account/info.html")
+
+@router_account.route("/set")
+def set():
+    return ops_render("/account/set.html")
